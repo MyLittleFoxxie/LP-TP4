081a736f5b47ea7179bf9ec39b47536dff2b46c3
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-04-05 20:43:05 -0300

    Update adjacente.pl

--- TP4/adjacente.pl
+++ TP4/adjacente.pl
@@ -1,3 +1,6 @@
+%Amanda Oliveira e Vitor Raposo
+%05-04-2021
+
 adjacente(X, Y, [X,Y|_]).   %Caso os dois elementos da cabeca sao adjacentes, true
 adjacente(X, Y, [_ | L]) :- %Caso contrario, procurar o resto da lista
     adjacente(X, Y, L).
