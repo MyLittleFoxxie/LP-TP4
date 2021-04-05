273c3ff70f59a04963b1fc8aefbf5411bcabd1c1
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-04-05 20:35:44 -0300

    Update ultimo.pl

--- TP4/ultimo.pl
+++ TP4/ultimo.pl
@@ -1,5 +1,5 @@
-/Obter o último elemento de uma lista
-Amanda Oliveira e Vitor Raposo
-05-04-2021/
+%Obter o ultimo elemento de uma lista
+%Amanda Oliveira e Vitor Raposo
+%05-04-2021
 ultimo([A | []], S).
 ultimo([_ | O], P):- ultimo(O,P).
