a193547af0db05c07d8b1537d82051b7e60f23be
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-04-05 14:57:17 -0300

    Update rotacionarn.pl

--- TP4/rotacionarn.pl
+++ TP4/rotacionarn.pl
@@ -1,10 +1,10 @@
 rotacionarn(0, L, L) :- !.
 rotacionarn(N, L1, L2) :-
-  concatenar(T, [H], L) rotacionar(L1, L), N1 is N-1, rotacionarn(N1, L, L2).
+  andar(L1, L), N1 is N-1, rotacionarn(N1, L, L2).
 
-rotacionar([H|T], L) :- 
-    concatenar(T, [H], L).
+andar([X | Rest], L) :- 
+    concatenar(Rest, [X], L).
 
 concatenar([], L, L).
-concatenar([X|L1], L2, [X|L3]) :- 
-    concatenar(L1, L2, L3).
+concatenar([X | Rest1], Rest2, [X | Rest3]) :- 
+    concatenar(Rest1, Rest2, Rest3).
