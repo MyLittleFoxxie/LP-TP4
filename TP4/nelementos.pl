b400c0af6989684cff989c6fc9852cf03171159e
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-04-05 20:34:27 -0300

    Update nelementos.pl

--- TP4/nelementos.pl
+++ TP4/nelementos.pl
@@ -1,6 +1,6 @@
-/Obter o número	de	elementos de uma lista
-Amanda Oliveira e Vitor Raposo
-05-04-2021/
+%Obter o numero	de	elementos de uma lista
+%Amanda Oliveira e Vitor Raposo
+%05-04-2021
 
 nelementos([],0).
 nelementos([_ | A], S):- nelementos(A,O), S is (O+1).
