1eb06810aabf0e35354ee0c5626fb49e8a9bf381
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-04-05 18:39:53 -0300

    Update inserirfim.pl

--- TP4/inserirfim.pl
+++ TP4/inserirfim.pl
@@ -1,3 +1,3 @@
-inserirfim(X, [], [X]).
-inserirfim(X, [Y | Rest1], [Y | Rest2]) :- 
-    inserirfim(X, Rest1, Rest2).
+inserirfim(X, [], [X]).                 %Caso a lista seja vazia, resultado contem somente um elemento
+inserirfim(X, [Y | L1], [Y | L2]) :-    %Caso contrario, resultado sera uma nova lista com a mesma cabeca
+    inserirfim(X, L1, L2).
