2f23eae0901c2923cc44eedd30a167de659d1556
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-04-05 20:43:40 -0300

    Update maior.pl

--- TP4/maior.pl
+++ TP4/maior.pl
@@ -1,3 +1,6 @@
+%Amanda Oliveira e Vitor Raposo
+%05-04-2021
+
 maior([X], X).				%Caso a lista tenha somente um elemento, esse é o maximo
 maior([X,Y | L], Max) :-	%Caso contrario, comparar o primeiro elemento ao resto maximo (LMax)
 	maior([Y | L], LMax),
