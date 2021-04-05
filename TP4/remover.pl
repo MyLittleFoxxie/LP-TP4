e315e5338c479eb5aa5cf9503cc22dbabacc7160
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-04-05 18:40:31 -0300

    Update remover.pl

--- TP4/remover.pl
+++ TP4/remover.pl
@@ -1,5 +1,5 @@
-remover(_, [], []).
-remover(X, [X | T], L) :- 
-    remover(X, T, L), !.
-remover(X, [H | T], [H | L]) :- 
-    remover(X, T, L ).
+remover(_, [], []).                 %Caso a lista vazia, retorna vazio
+remover(X, [X | L1], L) :-          %Caso o elemento seja a cabeca da lista, nao copiar no resultado
+    remover(X, L1, L), !.
+remover(X, [Y | L1], [Y | L]) :-    %Caso contrario, copiar a cabeca para o resultado
+    remover(X, L1, L).
