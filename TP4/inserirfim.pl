ae2d46b4e445f69591d525ac48747ac4b33211cc
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-04-05 20:43:15 -0300

    Update inserirfim.pl

--- TP4/inserirfim.pl
+++ TP4/inserirfim.pl
@@ -1,3 +1,6 @@
+%Amanda Oliveira e Vitor Raposo
+%05-04-2021
+
 inserirfim(X, [], [X]).                 %Caso a lista seja vazia, resultado contem somente um elemento
 inserirfim(X, [Y | L1], [Y | L2]) :-    %Caso contrario, resultado sera uma nova lista com a mesma cabeca
     inserirfim(X, L1, L2).
