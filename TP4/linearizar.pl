fda1cc276de579ea6186e7a0e6694a6b17850672
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-04-05 19:22:27 -0300

    Update linearizar.pl

--- TP4/linearizar.pl
+++ TP4/linearizar.pl
@@ -1,8 +1,7 @@
-%EM CONSTRUCAO
-
-my_flatten(X,[X]).
-my_flatten([],[]).
-my_flatten([H|T],R) :-
-    my_flatten(H,HFlat),
-    my_flatten(T,TFlat),
-    append(HFlat,TFlat,R).
+linearizar([X | []], X).    %Caso a lista vazia, retorne vazio
+linearizar([X | [Y | L1]], L2) :-   %Concatena o segundo elemento da cabeca (Y), que também é o primeiro
+    concatenar(X, Y, Z), linearizar([Z | L1], L2).  %elemento da segunda lista enraizada, em uma nova lista L2
+                                                   
+concatenar([], L, L).
+concatenar([X | L1], L2, [X | L3]) :- 
+    concatenar(L1, L2, L3).
