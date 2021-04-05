36cbac5995777073f1aa560eaa2bd44306e57ac1
Amanda Oliveira <61570022+solariius@users.noreply.github.com>
2021-04-05 19:59:50 -0300

    Add files via upload

---
+++ TP4/nelementos.pl
@@ -0,0 +1,6 @@
+/Obter o número	de	elementos de uma lista
+Amanda Oliveira e Vitor Raposo
+05-04-2021/
+
+nelementos([],0).
+nelementos([_ | A], S):- nelementos(A,O), S is (O+1).
---
+++ TP4/ultimo.pl
@@ -0,0 +1,5 @@
+/Obter o último elemento de uma lista
+Amanda Oliveira e Vitor Raposo
+05-04-2021/
+ultimo([A | []], S).
+ultimo([_ | O], P):- ultimo(O,P).
