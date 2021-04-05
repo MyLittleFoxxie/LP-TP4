b3e0a60d7bc79c3b5b7eb14f371c21efaeeb0df4
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-04-05 18:40:42 -0300

    Update rotacionarn.pl

--- TP4/rotacionarn.pl
+++ TP4/rotacionarn.pl
@@ -1,10 +1,10 @@
-rotacionarn(0, L, L) :- !.
-rotacionarn(N, Y, Z) :-
-  andar(Y, L), N1 is N-1, rotacionarn(N1, L, Z).
+rotacionarn(0, L, L) :- !.  %Caso n seja 0, nao rotacionar
+rotacionarn(N, Y, Z) :-     %Caso contrario, utilizar N como um contador
+  andar(Y, L), N1 is N-1, rotacionarn(N1, L, Z). %Chama a funcao andar, diminui o contador e percorre o restante da lista
 
-andar([X | Rest], L) :- 
-    concatenar(Rest, [X], L).
+andar([X | T], L) :-          %Concatena o segundo elemento da cabeca da lista anterior na lista resultado
+    concatenar(T, [X], L).  
 
 concatenar([], L, L).
-concatenar([X | Rest1], Rest2, [X | Rest3]) :- 
-    concatenar(Rest1, Rest2, Rest3).
+concatenar([X | L1], L2, [X | L3]) :- 
+    concatenar(L1, L2, L3).
