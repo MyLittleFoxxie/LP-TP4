6130983227098190d26c9a22ff3bd3bc8ff6f8e0
MyLittleFoxxie <78807369+MyLittleFoxxie@users.noreply.github.com>
2021-04-05 21:58:57 -0300

    Update ordenar.pl

--- TP4/ordenar.pl
+++ TP4/ordenar.pl
@@ -9,5 +9,5 @@ tirar(A,[O|L],[O|P]):- A\==O,tirar(A,L,P).
 
 menor([X|[]],X).
 menor([A|[O|L]],V):- A<O, menor([A|L],R), V is R.
-menor([A|[O|L]],V):- A>=O, menor([O|L],R), V is R..
+menor([A|[O|L]],V):- A>=O, menor([O|L],R), V is R.
 
