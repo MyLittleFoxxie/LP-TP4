c67767b1bb284566485f807fe584c1362a73955c
Amanda Oliveira <61570022+solariius@users.noreply.github.com>
2021-04-05 21:44:26 -0300

    Add files via upload

---
+++ TP4/ordenar.pl
@@ -0,0 +1,13 @@
+/*Ordenar uma lista de inteiros.
+Amanda Oliveira e Vitor Raposo
+05-04-2021*/
+odenar([],[]).
+odenar(L,[A|LAUX]):- menor(L,A),tirar(A,L,LAUX2),ordenar(LAUX2,LAUX).
+
+tirar(A,[A|L],L).
+tirar(A,[O|L],[O|P]):- A\==O,tirar(A,L,P).
+
+menor([X|[]],X).
+menor([A|[O|L]],V):- A<O, menor([A|L],R), V is R.
+menor([A|[O|L]],V):- A>=O, menor([O|L],R), V is R..
+
